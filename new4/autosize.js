03fc458e0963d24e7a9e31e1a1ca5c379c795d9c
Ethan Steinberg <ethan.steinberg@gmail.com>
2011-06-13 21:24:48 -0700

    Default for autosize

--- new4/autosize.js
+++ new4/autosize.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
    var targetHeight = $(window).height() -250;
+
+   if (targetHeight < 450)
+      targetHeight = 450;
+
    $("#realContent").height(targetHeight); 
 });
